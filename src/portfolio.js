9871e58aaf54ebb81f24af8c0a3bac8b86813fc0
KiranKumarMalik <malikkiran413@gmail.com>
2025-07-04 09:58:36 +0530

    technical del

--- src/portfolio.js
+++ src/portfolio.js
@@ -9,7 +9,7 @@ const settings = {
 const seo = {
   title: "Kiran Kumar Malik",
   description:
-    "A passionate who always thrives which develop and scalable and technical to.",
+    "A passionate who always thrives which develop and scalable and to.",
   og: {
     title: "Kiran Kumar Malik",
     type: "website",
